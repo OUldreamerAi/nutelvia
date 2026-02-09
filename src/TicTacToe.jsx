416067d6efbc537631addde54188c03f1f7c5484
olli.becker <olli10856@gmail.com>
2026-02-09 21:07:49 +0200

    i am working on the main os part creating popups

--- src/TicTacToe.jsx
+++ src/TicTacToe.jsx
@@ -32,7 +32,8 @@ export default function Board() {
     
     return (
     <>
-      <div className="board-row">
+        <div className="status">{status}</div>
+        <div className="board-row">
         <Square value={squares[0]} onSquareClick={() => handleClick(0)}/>
       </div>
       <div className="board-row">
